46e468cac0f58d64a17451efc6d62a0a27768e04
João <jsoareez@gmail.com>
2020-07-13 20:16:49 -0300

    gatsby link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import { Link } from 'gatsby';
+
 export default function AboutPage() {
   return (
-    <h1>About Page</h1>
+    <>
+      <h1>About Page</h1>
+      <ul>
+        <li>
+          <a href="/">Home Page</a>
+        </li>
+        <li>
+          <Link to="/about" activeStyle={{ color: 'blue' }}>About</Link>
+        </li>
+        <li>
+          <Link to="/">Home Page(Gatsby Link)</Link>
+        </li>
+      </ul>
+    </>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { Link } from 'gatsby';
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -7,6 +9,17 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
+    <ul>
+      <li>
+        <a href="/about">About Page</a>
+      </li>
+      <li>
+        <Link to="/" activeStyle={{ color: 'red' }}>Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About Page(Gatsby Link)</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
