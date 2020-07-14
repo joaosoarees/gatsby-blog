fb9c39e2c9eeebde59735b4ed0788383cb4c2719
Joao Vitor <smart@LAPTOP-P7HN92NI.localdomain>
2020-07-14 12:13:57 -0300

    installed styled-icons

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,12 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/joaosoarees'
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://www.linkedin.com/in/jo%C3%A3o-vitor-alves-soares-a20302194/'
+  }
+];
+
+export default links;
