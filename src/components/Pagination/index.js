22fe9e186f3a5dacdf8dea0eafdc54af8e86ba40
Joao Vitor <smart@LAPTOP-P7HN92NI.localdomain>
2020-07-16 10:29:56 -0300

    create Pagination component

---
+++ src/components/Pagination/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+import { Link } from 'gatsby';
+
+import { PaginationWrapper } from './styles';
+
+export default function Pagination({ 
+  isFirst, 
+  isLast, 
+  currentPage, 
+  numPages, 
+  prevPage, 
+  nextPage 
+}) {
+  return (
+    <PaginationWrapper>
+      {!isFirst && 
+        <Link to={prevPage}>← página anterior</Link>
+      }
+
+      <p>{currentPage} de {numPages}</p>
+
+      {!isLast && 
+        <Link to={nextPage}>proxima página →</Link>
+      }
+    </PaginationWrapper>
+  );
+};
+
+Pagination.propTypes = {
+  isFirst: propTypes.bool.isRequired,
+  isLast: propTypes.bool.isRequired,
+  currentPage: propTypes.number.isRequired,
+  numPages: propTypes.number.isRequired,
+  prevPage: propTypes.string,
+  nextPage: propTypes.string,
+};
